71212a485f8ccda395db226da6e713bd4cf47065
Pierre-Antoine Champin <pchampin@liris.cnrs.fr>
2016-03-17 21:21:13 +0100

    fixed regexp for links in triangle-brackets

--- gereco.js
+++ gereco.js
@@ -45,7 +45,7 @@
                 html = html.replace(/"([^">\n]+)"/g, '"<a href="$1">$1</a>"');
             } else {
                 html = html.replace(/&lt;&gt;/g, '<a href="">&lt;&gt;</a>');
-                html = html.replace(/&lt;([^&\n]+)&gt;/g, '&lt;<a href="$1">$1</a>&gt;');
+                html = html.replace(/&lt;([^\n]+?)&gt;/g, '&lt;<a href="$1">$1</a>&gt;');
             }
             response.innerHTML = html;
         }
